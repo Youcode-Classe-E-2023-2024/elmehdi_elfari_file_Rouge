2722a333e414d654710a8160e79566a35c9a4b6d
ELMEHDI_ELFARI <144785903+ElmehdiElfari@users.noreply.github.com>
2024-03-19 01:51:45 +0000

    login

--- train/app/Http/Controllers/Auth/LoginController.php
+++ train/app/Http/Controllers/Auth/LoginController.php
@@ -27,13 +27,16 @@ class LoginController extends Controller
             $request->session()->regenerate();
             $user = User::where('id', Auth::id())->first();
 
-            if ($user->HasRole('admin')) {
+
+
+
+         /*   if ($user->HasRole('admin')) {
                 return redirect('dashboard')->with("login", 'true');
             } elseif ($user->hasRole('organisateur')) {
                 return redirect()->route('clients')->with("login", 'true');
             } else {
                 return redirect('login')->with("login", 'true');
-            }
+            }*/
         }
         return back()->withErrors(['email' => 'Invalid Credentials'])
             ->onlyInput();
