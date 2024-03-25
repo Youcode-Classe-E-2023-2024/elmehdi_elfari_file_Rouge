17047387757641991f61b5513bc167b9daa6eaac
ELMEHDI_ELFARI <144785903+ElmehdiElfari@users.noreply.github.com>
2024-03-25 10:12:51 +0000

    edit in controller home

--- project_file_rouge/app/Http/Controllers/HomeController.php
+++ project_file_rouge/app/Http/Controllers/HomeController.php
@@ -3,14 +3,16 @@
 namespace App\Http\Controllers;
 
 use App\Models\City;
+use App\Models\Parcours;
 use Illuminate\Http\Request;
 
 class HomeController extends Controller
 {
     public function welcome(){
 
+        $parcours = Parcours::with('City_depart','City_arrive')->get();
         $cities = City::all();
-        return view('pages.welcome' ,compact('cities'));
+        return view('pages.welcome', compact('parcours','cities'));
     }
 
 }
