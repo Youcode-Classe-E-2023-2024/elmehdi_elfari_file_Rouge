119f4a004c3a88f4bbcf9b3b30fba518d7da775c
ELMEHDI_ELFARI <144785903+ElmehdiElfari@users.noreply.github.com>
2024-03-28 12:23:07 +0000

    edit in controller dashboard

--- project_file_rouge/app/Http/Controllers/DashboardController.php
+++ project_file_rouge/app/Http/Controllers/DashboardController.php
@@ -4,21 +4,44 @@ namespace App\Http\Controllers;
 
 use App\Models\City;
 use App\Models\Parcours;
+use App\Models\User;
 use Illuminate\Http\Request;
 
 class DashboardController extends Controller
 {
     public function index(){
-
         $parcours = Parcours::count();
+
         $city = City::count();
-        return view('pages.dashboard' ,compact('parcours','city'));
+
+        $bannedUsersCount = User::where('status', 'ban')->count();
+
+        $unbannedUsersCount = User::where('status', 'unban')->count();
+
+        return view('pages.dashboard', compact('parcours', 'city', 'bannedUsersCount', 'unbannedUsersCount'));
     }
 
-    public function table(){
 
-        return view('pages.table');
+    public function table()
+    {
+        $users = User::all();
+
+        return view('pages.table', compact('users'));
     }
+
+    public function toggleStatus(User $user)
+    {
+        if ($user->status === 'ban') {
+            $user->update(['status' => 'unban']);
+            $message = 'User unbanned successfully';
+        } else {
+            $user->update(['status' => 'ban']);
+            $message = 'User banned successfully';
+        }
+
+        return redirect()->back()->with('message', $message);
+    }
+
 
     public function reservation(){
 
