03283c6eeca5180eb30216082bf9b66102b5cc99
ELMEHDI_ELFARI <144785903+ElmehdiElfari@users.noreply.github.com>
2024-03-28 04:26:12 +0000

    edit in page city

--- project_file_rouge/resources/views/pages/city.blade.php
+++ project_file_rouge/resources/views/pages/city.blade.php
@@ -149,7 +149,32 @@
                 </tr>
                 </thead>
                 <tbody>
-
+                @if(session('success'))
+                    <div class="bg-green-100 text-green-800 pl-4 pr-10 py-4 rounded relative" role="alert">
+                        <div class="inline-block max-sm:mb-2">
+                            <svg xmlns="http://www.w3.org/2000/svg" class="w-5 fill-green-500 inline mr-4" viewBox="0 0 512 512">
+                                <ellipse cx="256" cy="256" fill="#32bea6" data-original="#32bea6" rx="256" ry="255.832" />
+                                <path fill="#fff" d="m235.472 392.08-121.04-94.296 34.416-44.168 74.328 57.904 122.672-177.016 46.032 31.888z"
+                                      data-original="#ffffff" />
+                            </svg>
+                            <strong class="font-bold text-base">Success!</strong>
+                        </div>
+                        <span class="block sm:inline text-sm mx-4 max-sm:ml-0 max-sm:mt-1">{{ session('success') }}</span>
+                    </div>
+                @elseif(session('error'))
+                    <div class="bg-red-100 text-red-800 pl-4 pr-10 py-4 rounded relative" role="alert">
+                        <div class="inline-block max-sm:mb-2">
+                            <svg xmlns="http://www.w3.org/2000/svg" class="w-5 fill-red-500 inline mr-4" viewBox="0 0 32 32">
+                                <path
+                                    d="M16 1a15 15 0 1 0 15 15A15 15 0 0 0 16 1zm6.36 20L21 22.36l-5-4.95-4.95 4.95L9.64 21l4.95-5-4.95-4.95 1.41-1.41L16 14.59l5-4.95 1.41 1.41-5 4.95z"
+                                    data-original="#ea2d3f" />
+                            </svg>
+                            <strong class="font-bold text-base">Error!</strong>
+                        </div>
+                        <span class="block sm:inline text-sm mx-4 max-sm:ml-0 max-sm:mt-1">{{ session('error') }}</span>
+                    </div>
+                @endif
+                <br>
                 @foreach($ville as $city)
                     <tr>
                         <td class="border-b p-2">{{ $city->id }}</td>
