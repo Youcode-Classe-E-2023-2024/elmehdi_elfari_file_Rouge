c7e94f172af039adcc278642c320e7c716981f90
ELMEHDI_ELFARI <144785903+ElmehdiElfari@users.noreply.github.com>
2024-04-02 09:38:08 +0000

    edit in controller home

--- project_file_rouge/app/Http/Controllers/HomeController.php
+++ project_file_rouge/app/Http/Controllers/HomeController.php
@@ -2,16 +2,63 @@
 
 namespace App\Http\Controllers;
 
+use Illuminate\Http\Request;
 use App\Models\City;
 use App\Models\Parcours;
-use Illuminate\Http\Request;
 
 class HomeController extends Controller
 {
-    public function welcome(){
-
-        $parcours = Parcours::with('City_depart','City_arrive')->get();
+    public function welcome()
+    {
         $cities = City::all();
+        $parcours = Parcours::with('City_depart', 'City_arrive')->get();
+        return view('pages.welcome', compact('parcours', 'cities'));
+    }
+
+    public function search(Request $request)
+    {
+        $searchQuery = $request->input('query');
+        $departId = $request->input('depart_id');
+        $arriveId = $request->input('arrive_id');
+
+        $cities = City::all();
+
+        $parcours = Parcours::query();
+
+        if ($searchQuery) {
+            $parcours->where(function($query) use ($searchQuery) {
+                $query->where('title', 'LIKE', '%' . $searchQuery . '%')
+                    ->orWhereHas('City_depart', function($query) use ($searchQuery) {
+                        $query->where('name', 'LIKE', '%' . $searchQuery . '%');
+                    })
+                    ->orWhereHas('City_arrive', function($query) use ($searchQuery) {
+                        $query->where('name', 'LIKE', '%' . $searchQuery . '%');
+                    });
+            });
+        }
+
+        if ($departId) {
+            $parcours->where('depart_id', $departId);
+        }
+
+        if ($arriveId) {
+            $parcours->where('arrive_id', $arriveId);
+        }
+
+        $parcours = $parcours->with('City_depart', 'City_arrive')->paginate(6);
+
+        return view('pages.welcome', compact('parcours', 'cities'));
+    }
+
+    public function filter(Request $request){
+        $start_date = $request->input('start_date');
+        $end_date = $request->input('end_date');
+        $cities = City::all();
+
+        $parcours = Parcours::whereDate('created_at', '>=', $start_date)
+            ->whereDate('created_at', '<=', $end_date)
+            ->paginate(6);
+
         return view('pages.welcome', compact('parcours','cities'));
     }
 
