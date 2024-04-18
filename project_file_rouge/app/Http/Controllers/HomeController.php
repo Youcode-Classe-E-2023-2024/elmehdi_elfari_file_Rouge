5465222e98b3bc8caad5c59baddf22383de32aa2
ELMEHDI_ELFARI <144785903+ElmehdiElfari@users.noreply.github.com>
2024-04-18 11:24:40 +0100

    edit in controller home

--- project_file_rouge/app/Http/Controllers/HomeController.php
+++ project_file_rouge/app/Http/Controllers/HomeController.php
@@ -14,32 +14,18 @@ class HomeController extends Controller
         $cities = City::all();
         $parcours = Parcours::with('City_depart', 'City_arrive')->get();
 
-        return view('pages.welcome', compact( 'parcours', 'cities'));
+        return view('pages.home', compact('parcours', 'cities'));
     }
 
     public function search(Request $request)
     {
-        $searchQuery = $request->input('query');
         $departId = $request->input('depart_id');
         $arriveId = $request->input('arrive_id');
-        $start_date = $request->input('start_date');
-        $end_date = $request->input('end_date');
+        $depart_date = $request->input('depart_date');
+        $arrive_date = $request->input('arrive_date');
 
         $cities = City::all();
-
         $parcours = Parcours::query()->with('City_depart', 'City_arrive');
-
-        if ($searchQuery) {
-            $parcours->where(function($query) use ($searchQuery) {
-                $query->where('title', 'LIKE', '%' . $searchQuery . '%')
-                    ->orWhereHas('City_depart', function($query) use ($searchQuery) {
-                        $query->where('name', 'LIKE', '%' . $searchQuery . '%');
-                    })
-                    ->orWhereHas('City_arrive', function($query) use ($searchQuery) {
-                        $query->where('name', 'LIKE', '%' . $searchQuery . '%');
-                    });
-            });
-        }
 
         if ($departId) {
             $parcours->where('depart_id', $departId);
@@ -49,15 +35,10 @@ class HomeController extends Controller
             $parcours->where('arrive_id', $arriveId);
         }
 
-        if ($start_date && $end_date) {
-            $parcours->whereBetween('created_at', [$start_date, $end_date]);
-        }
+        $parcours->whereDate('depart_date', $depart_date);
 
         $parcours = $parcours->get();
 
-        return view('pages.welcome', compact('parcours', 'cities'));
+        return view('pages.home', compact('parcours', 'cities'));
     }
-
-
-
 }
