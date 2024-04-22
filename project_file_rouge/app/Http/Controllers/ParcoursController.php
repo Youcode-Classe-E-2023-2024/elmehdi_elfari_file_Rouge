71c40ee27e8f281cd8b04c6bbde37bf87a2ab801
ELMEHDI_ELFARI <144785903+ElmehdiElfari@users.noreply.github.com>
2024-04-22 09:14:44 +0100

    Edit in page controller parcours

--- project_file_rouge/app/Http/Controllers/ParcoursController.php
+++ project_file_rouge/app/Http/Controllers/ParcoursController.php
@@ -28,11 +28,12 @@ class ParcoursController extends Controller
         $request->validate([
             'depart_id' => 'required|integer|exists:cities,id',
             'arrive_id' => 'required|integer|exists:cities,id|different:depart_id',
-            'longeur_Parcour' => 'required|integer|max:255',
+            'distance_Parcour' => 'required|integer|max:255',
             'Prix_Parcour' => 'required|integer|max:255',
             'nbr_place' => 'required|integer|max:255',
+            'classes' => 'required|integer|in:1,2',
             'time_depart' => 'required|date_format:H:i',
-            'depart_date' => 'required|date_format:Y-m-d',
+            'depart_date' => 'required|date|after_or_equal:today',
             'arrive_time' => 'required|date_format:H:i|after:time_depart',
         ]);
 
@@ -41,14 +42,14 @@ class ParcoursController extends Controller
 
         $duree = $time_depart->diff($arrive_time)->format('%Hh %Im');
 
-
         $depart_date = \Carbon\Carbon::createFromFormat('Y-m-d', $request->depart_date)->toDateString();
 
         $validated = [
             'depart_id' => $request->depart_id,
             'arrive_id' => $request->arrive_id,
-            'longeur_Parcour' => $request->longeur_Parcour,
+            'distance_Parcour' => $request->distance_Parcour,
             'Prix_Parcour' => $request->Prix_Parcour,
+            'classes' => $request->classes,
             'nbr_place' => $request->nbr_place,
             'time_depart' => $time_depart,
             'arrive_time' => $arrive_time,
@@ -73,11 +74,12 @@ class ParcoursController extends Controller
         $request->validate([
             'depart_id' => 'required',
             'arrive_id' => 'required',
-            'longeur_Parcour' => 'required|integer|max:255',
+            'distance_Parcour' => 'required|integer|max:255',
             'Prix_Parcour' => 'required|integer|max:255',
             'nbr_place' => 'required|integer|max:255',
+            'classes' => 'required|integer|in:1,2',
             'time_depart' => 'required|date_format:H:i',
-            'depart_date' => 'required|date_format:Y-m-d',
+            'depart_date' => 'required|date|after_or_equal:today',
             'arrive_time' => 'required|date_format:H:i|after:time_depart',
         ]);
 
@@ -88,11 +90,14 @@ class ParcoursController extends Controller
 
         $duree = $time_depart->diff($arrive_time)->format('%Hh %Im');
 
+
+
         $parcour->fill([
             'depart_id' => $request->depart_id,
             'arrive_id' => $request->arrive_id,
-            'longeur_Parcour' => $request->longeur_Parcour,
+            'distance_Parcour' => $request->distance_Parcour,
             'Prix_Parcour' => $request->Prix_Parcour,
+            'classes' => $request->classes,
             'nbr_place' => $request->nbr_place,
             'time_depart' => $time_depart,
             'arrive_time' => $arrive_time,
@@ -111,5 +116,19 @@ class ParcoursController extends Controller
         $parcours->delete();
 
         return redirect()->back()->with('success', 'Parcours deleted successfully.');
+    }
+
+    public function parcours($class)
+    {
+        $parcours = Parcours::where('classes', $class)->get();
+
+        if(is_null($parcours))
+            return response()->json(['status'=> 'no data'], 302);
+
+        return response()->json([
+            'status' => 'data founded',
+            'parcours' => $parcours
+        ]);
+
     }
 }
